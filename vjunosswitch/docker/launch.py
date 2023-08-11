fe6ff2dda52e5018210f629f65008ca914f02a34
akielaries <akiel@akiel.org>
2023-08-11 15:34:51 -0700

    vJunos-switch updates

--- vjunosswitch/docker/launch.py
+++ vjunosswitch/docker/launch.py
@@ -6,9 +6,18 @@ import os
 import re
 import signal
 import sys
+<<<<<<< HEAD
 
 import vrnetlab
 
+=======
+import uuid
+
+import vrnetlab
+
+STARTUP_CONFIG_FILE = "/config/startup-config.cfg"
+
+>>>>>>> more patches
 def handle_SIGCHLD(signal, frame):
     os.waitpid(-1, os.WNOHANG)
 
@@ -34,11 +43,30 @@ class VJUNOSSWITCH_vm(vrnetlab.VM):
             if re.search(".qcow2$", e):
                 disk_image = "/" + e
         super(VJUNOSSWITCH_vm, self).__init__(username, password, disk_image=disk_image, ram=5120)
+<<<<<<< HEAD
+=======
+        # mount config.img disk with juniper.conf file
+        #self.qemu_args.extend(["-blockdev", '{"driver":"file","filename":"/var/aaries/clab/test_docker/config.qcow2","node-name":"libvirt-1-storage","cache":{"direct":false,"no-flush":false},"auto-read-only":true,"discard":"unmap"}'])
+        
+        #self.qemu_args.extend(["-device", "virtio-blk-pci,scsi=off,bus=pci.0,addr=0x2,drive=libvirt-2-format,id=virtio-disk0,bootindex=1,write-cache=on"])
+        
+        #self.qemu_args.extend(["-blockdev", "node-name=libvirt-1-storage,driver=raw,file.driver=host_device,file.filename=/config.img"])
+        #self.qemu_args.extend(["-blockdev", "node-name=libvirt-1-storage,driver=raw,file.filename=/config.img,file.driver=file"])
+
+        #self.qemu_args.extend(["-device", "usb-storage,bus=usb.0,port=1,drive=libvirt-1-format,id=usb-disk0,removable=off,write-cache=on"])
+
+>>>>>>> more patches
         self.qemu_args.extend(["-smp", "4,sockets=1,cores=4,threads=1"])
         # Additional CPU info
         self.qemu_args.extend([
             "-cpu", "IvyBridge,vme=on,ss=on,vmx=on,f16c=on,rdrand=on,hypervisor=on,arat=on,tsc-adjust=on,umip=on,arch-capabilities=on,pdpe1gb=on,skip-l1dfl-vmentry=on,pschange-mc-no=on,bmi1=off,avx2=off,bmi2=off,erms=off,invpcid=off,rdseed=off,adx=off,smap=off,xsaveopt=off,abm=off,svm=off"
             ])
+<<<<<<< HEAD
+=======
+        # generate random uuid
+        #self.qemu_args.extend(["-uuid", str(uuid.uuid4())]
+        #self.qemu_args.extend(["-uuid", "324c9ca0-14d6-4cf3-b68d-97a0a38006bb"])
+>>>>>>> more patches
         self.qemu_args.extend(["-overcommit", "mem-lock=off"])
         self.qemu_args.extend(["-display", "none", "-no-user-config", "-nodefaults", "-boot", "strict=on"])
         self.nic_type = "virtio-net-pci"
@@ -76,6 +104,10 @@ class VJUNOSSWITCH_vm(vrnetlab.VM):
                 # login, then execute basic init config items
                 # run main config!
                 #self.bootstrap_config()
+<<<<<<< HEAD
+=======
+                #self.startup_config()
+>>>>>>> more patches
                 # close telnet connection
                 self.tn.close()
                 # startup time?
@@ -96,6 +128,7 @@ class VJUNOSSWITCH_vm(vrnetlab.VM):
 
         return
 
+<<<<<<< HEAD
     def config(self):
         pass
 
@@ -121,6 +154,49 @@ class VJUNOSSWITCH_vm(vrnetlab.VM):
         self.wait_write("commit")
         #self.wait_write("set system services ssh", "#")
         #self.wait_write("set system services netconf ssh", "#")
+=======
+    def startup_config(self):
+        """Load additional config provided by user."""
+
+        if not os.path.exists(STARTUP_CONFIG_FILE):
+            self.logger.trace(f"Startup config file {STARTUP_CONFIG_FILE} is not found")
+            return
+
+        self.logger.trace(f"Startup config file {STARTUP_CONFIG_FILE} exists")
+        with open(STARTUP_CONFIG_FILE) as file:
+            config_lines = file.readlines()
+            config_lines = [line.rstrip() for line in config_lines]
+            self.logger.trace(f"Parsed startup config file {STARTUP_CONFIG_FILE}")
+
+        self.logger.info(f"Writing lines from {STARTUP_CONFIG_FILE}")
+
+        self.wait_write("cli", "#")
+        self.wait_write("configure", ">")
+        # Apply lines from file
+        for line in config_lines:
+            self.wait_write(line)
+
+        self.wait_write("commit")
+        self.wait_write("exit")
+
+    def bootstrap_config(self):
+        """ Do the actual bootstrap config using send and wait
+        """
+        self.logger.info("applying bootstrap configuration")
+        self.wait_write("cli", "#") 
+        self.wait_write("configure", ">")
+        self.wait_write("delete chassis auto-image-upgrade, #")
+        self.wait_write("set system login user %s class super-user authentication plain-text-password" % ( self.username ), "#")
+        self.wait_write(self.password)#, "New password: ")
+        self.wait_write(self.password)#, "Retype new password: ")
+        self.wait_write("set system root-authentication plain-text-password", "#")
+        self.wait_write(self.password)#, "New password: ")
+        self.wait_write(self.password)#, "Retype new password: ")
+        self.wait_write("delete chassis auto-image-upgrade")
+        self.wait_write("commit")
+        self.wait_write("set system services ssh", "#")
+        self.wait_write("set system services netconf ssh", "#")
+>>>>>>> more patches
         # remove DHCP6 configuration before setting DHCP4
         self.wait_write("delete interfaces fxp0 unit 0 family inet6")
         # set interface fxp0  on dedicated management vrf, to avoid 
